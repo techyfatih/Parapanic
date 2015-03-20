fcb4b379a9ab78d79e754cdc914992cd775746d1
heartles <jbmdev@outlook.com>
2015-03-20 11:40:47 -0700

    Split ambulance.cs into car and ambulance Should make it easier when we have multiple cars in the game.

--- Parapanic/GameName1/Ambulance.cs
+++ Parapanic/GameName1/Ambulance.cs
@@ -10,21 +10,8 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Parapanic
 {
-    class Ambulance
+    class Ambulance : Car
     {
-        public Vector2 position;
-        public float rotation;
-        public Vector2 origin;
-
-        Rectangle carRect;
-        double topSpeed;
-        double acceleration;
-        double direction;
-        double friction;
-        double mouseDirection;
-        double speed;
-        const double maxTurnrate = 3;
-
         public Ambulance(int x, int y, double direction, double topSpeed, double acceleration, double friction)
         {
             position = new Vector2(x, y);
@@ -37,7 +24,7 @@ namespace Parapanic
             speed = 0;
         }
 
-        public void Update(GameTime gameTime, World world)
+        public override void Update(GameTime gameTime, World world)
         {
             //Left Click - Acceleration
                 Console.WriteLine(speed);
@@ -90,30 +77,8 @@ namespace Parapanic
             //Slide according to friction is nothing is pressed
             if (Mouse.GetState().LeftButton == ButtonState.Released && Mouse.GetState().RightButton == ButtonState.Released)
                 speed *= friction;
-
-            Vector2 a = new Vector2((float)Math.Sin(MathHelper.ToRadians((float)direction)),
-                                    (float)Math.Cos(MathHelper.ToRadians((float)direction)));
-            a.Normalize();
-
-            float newY = position.Y + (int)(a.X * speed);
-            float newX = position.X + (int)(a.Y * speed);
-            foreach (Block b in world.grid)
-            {
-                if (b.GetType().Equals(typeof(WallBlock)))
-                {
-                    if (newY >= b.position.Y && newY <= b.position.Y + Block.size)
-                        if (newX >= b.position.X && newX <= b.position.X + Block.size)
-                            newX = position.X;
-                    if (newX >= b.position.X && newX <= b.position.X + Block.size)
-                        if (newY >= b.position.Y && newY <= b.position.Y + Block.size)
-                            newY = position.Y;
-                }
-            }
-            position.Y = newY;
-            position.X = newX;
-            carRect.Y = (int)newY;
-            carRect.X = (int)newX;
-            rotation = MathHelper.ToRadians((float)direction);
+            
+            base.Update(gameTime, world);
         }
     }
 }
---
+++ Parapanic/GameName1/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Parapanic
+{
+    class Car
+    {
+        public Vector2 position;
+        public float rotation;
+        public Vector2 origin;
+
+        protected Rectangle carRect;
+        protected double topSpeed;
+        protected double acceleration;
+        protected double direction;
+        protected double friction;
+        protected double mouseDirection;
+        protected double speed;
+        protected const double maxTurnrate = 3;
+
+        public virtual void Update(GameTime gameTime, World world)
+        {
+            Vector2 a = new Vector2((float)Math.Sin(MathHelper.ToRadians((float)direction)),
+                                    (float)Math.Cos(MathHelper.ToRadians((float)direction)));
+            a.Normalize();
+
+            float newY = position.Y + (int)(a.X * speed);
+            float newX = position.X + (int)(a.Y * speed);
+            foreach (Block b in world.grid)
+            {
+                if (b.GetType().Equals(typeof(WallBlock)))
+                {
+                    if (newY >= b.position.Y && newY <= b.position.Y + Block.size)
+                        if (newX >= b.position.X && newX <= b.position.X + Block.size)
+                            newX = position.X;
+                    if (newX >= b.position.X && newX <= b.position.X + Block.size)
+                        if (newY >= b.position.Y && newY <= b.position.Y + Block.size)
+                            newY = position.Y;
+                }
+            }
+            position.Y = newY;
+            position.X = newX;
+            carRect.Y = (int)newY;
+            carRect.X = (int)newX;
+            rotation = MathHelper.ToRadians((float)direction);
+        }
+    }
+}
